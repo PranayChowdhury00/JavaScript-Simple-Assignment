fee5d270ce5340120547e68bcd77c571cbd06cd1
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2026-06-29 20:38:13 +0600

    this is my 2nd problem

--- assignment.js
+++ assignment.js
@@ -2,4 +2,17 @@ let area = 500;
 if(area>0 && area<=1000000000){
  // console.log(area= area/2);
 }
+ 
+// 2nd problem 
+let money =14000;
+if(money >= 25000){
+ console.log('laptop kine dibo')
+ 
+}
+else if(money>= 10000 && money <25000){
+ console.log('cycle kine dibo')
+}
+else{
+ console.log('chocolate kine dibo')
+}
 
